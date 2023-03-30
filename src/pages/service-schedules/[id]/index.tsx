e01a9666beeb9ea12635701b951cd77ec806340c
Kleber willian Dutra Freire <kleberfreire@MacBook-Air-de-Kleber.local>
2023-03-30 00:40:51 -0300

    layout edit pronto

--- src/pages/service-schedules/[id]/index.tsx
+++ src/pages/service-schedules/[id]/index.tsx
@@ -1,33 +1,316 @@
 import * as React from 'react';
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from 'react';
 
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useContext } from 'react';
-import { AuthContext } from '@/contexts/AuthContext';
 import { getSession } from 'next-auth/react';
 import { GetServerSideProps } from 'next/types';
+
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+
+
+import { ClientInfor, ClientVehicle, ServiceSchedulesListProps, TechnicalConsultant } from '@/types/service-schedule';
+import { apiCore } from '@/lib/api';
+
+
 import { useRouter } from 'next/router';
 
+import Title from '@/components/Title';
+
+import List from '@mui/material/List';
+
+import Stack from '@mui/material/Stack';
+
+import { ButtonCenter, ButtonLeft, ButtonRight, DateTimePickerCard, DividerCard, InfoCardName, InfoCardText, ListItemCard, TitleCard } from '@/styles/pages/service-schedules/stylesEdit';
+import Skeleton from '@mui/material/Skeleton';
+import PrintIcon from '@mui/icons-material/Print';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import ptBR from 'date-fns/locale/pt-BR';
+import dayjsPluginUTC from 'dayjs-plugin-utc'
 
 
-type SignInDataProps = {
-  username: string
-  password: string
-}
+const api = new apiCore()
 
-export default function CompanuId() {
+
+export default function ServiceSchedulesCreate() {
+  const [client, setClient] = useState<ClientInfor | null>()
+  const [clientVehicle, setClientVehicle] = useState<ClientVehicle | null>()
+  const [visitDate, setVisitDate] = useState<Dayjs | null>(null)
+  const [technicalConsultant, setTechnicalConsultant] = useState<TechnicalConsultant | null>()
 
   const router = useRouter()
 
-  const id = router.query.id
+
+  useEffect(() => {
+    const { id } = router.query
+    console.log(id)
+      api.get(`/service-schedule/${id}`)
+        .then((response) => {
+          console.log(response.data);
+
+          const {client, client_vehicle,technical_consultant, promised_date} = response.data.data
+          setClient({
+            name: client.name ?? 'Não informado',
+            cpf: client.document ?? 'Não informado',
+            email: client.email[0] ?? 'Não informado',
+            telefone: client.phone[0] ?? 'Não informado',
+            address: client.address ?? 'Não informado'
+          })
+          
+          setClientVehicle({
+            brand: client_vehicle.vehicle.model.brand.name,
+            chassis: client_vehicle.chasis,
+            vehicle: client_vehicle.vehicle.name,
+            model: `${client_vehicle.vehicle.model.name} - ${client_vehicle.vehicle.model_year}`,
+            color: client_vehicle.color,
+            plate: client_vehicle.plate
+          })
+          dayjs.extend(dayjsPluginUTC)
+          const promisedDate = dayjs(new Date(promised_date))
+          console.log(dayjs(promisedDate))
+          setVisitDate(promisedDate)
+
+          setTechnicalConsultant({
+            id: technical_consultant.id,
+            name: technical_consultant.name
+          })
+        }).catch((err) => { 
+          setClient(null)
+          setClientVehicle(null)
+          setTechnicalConsultant(null)
+        })
+    
+  },[router.query])
 
   return (
-      <Container component="main" maxWidth="xs">
-        <Typography variant="body2" color="text.secondary" align="center" >
-            agendamento {id}
-        </Typography>
-      </Container>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={12} lg={12}>
+          <Stack direction='row'>
+            <Title>Agenda de Serviços</Title>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} md={7} lg={7}>
+          <Stack spacing={3}>
+               {/* cliente */}
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+            <Stack direction='row'>
+              <TitleCard >cliente</TitleCard>
+              </Stack>
+                <DividerCard />
+            <List dense={false}>
+              
+              <ListItemCard>
+                  <InfoCardName>
+                    Nome:
+                  </InfoCardName> {client?.name ? (<InfoCardText >{client?.name}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                  <InfoCardName>
+                    CPF:
+                  </InfoCardName> {client?.cpf ? (<InfoCardText>{client?.cpf}</InfoCardText>) : (
+                     <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                    Telefone:
+                  </InfoCardName> {client?.telefone ? (<InfoCardText>{client?.telefone}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                    E-mail:
+                  </InfoCardName> {client?.email ? (<InfoCardText>{client?.email}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                   Endereço:
+                  </InfoCardName> {client?.address ? (<InfoCardText>{client?.address}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              
+            </List>
+            </Paper>
+               {/* Veículo */}
+              <Paper
+                    sx={{
+                      p: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                     <Stack direction='row'>
+              <TitleCard >Veículo</TitleCard>
+              </Stack>
+                <DividerCard />
+            <List dense={false}>
+              
+              <ListItemCard>
+                  <InfoCardName>
+                    Marca:
+                  </InfoCardName> {clientVehicle?.brand ? (<InfoCardText >{clientVehicle?.brand}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                  <InfoCardName>
+                    Modelo:
+                  </InfoCardName> {clientVehicle?.model ? (<InfoCardText>{clientVehicle?.model}</InfoCardText>) : (
+                     <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                    Veículo:
+                  </InfoCardName> {clientVehicle?.vehicle ? (<InfoCardText>{clientVehicle?.vehicle}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                    Cor:
+                  </InfoCardName> {clientVehicle?.color ? (<InfoCardText>{clientVehicle?.color}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                <InfoCardName>
+                   Chassi:
+                  </InfoCardName> {clientVehicle?.chassis ? (<InfoCardText>{clientVehicle?.chassis}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+                </ListItemCard>
+                 <ListItemCard>
+                <InfoCardName>
+                   Placa:
+                  </InfoCardName> {clientVehicle?.plate ? (<InfoCardText>{clientVehicle?.plate}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              
+            </List>
+            </Paper>
+            
+          </Stack>
+        </Grid>
+        
+        <Grid item xs={12} md={5} lg={5}>
+          <Stack spacing={2}>
+            <Stack spacing={2} direction='row' display='flex' justifyContent='center' >
+              <ButtonLeft >Listar Checklists</ButtonLeft>
+              <ButtonCenter ><PrintIcon /></ButtonCenter>
+              <ButtonRight startIcon={<AddCircleOutlineIcon/>}>Novo</ButtonRight>
+            </Stack>
+            <Stack spacing={2} direction='row' display='flex' justifyContent='center' >
+              <ButtonLeft >Listar Orçamentos</ButtonLeft>
+              <ButtonCenter ><PrintIcon /></ButtonCenter>
+              <ButtonRight startIcon={<AddCircleOutlineIcon/>}>Novo</ButtonRight>
+            </Stack>
+            {/* Agendamento */}
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <Stack direction='row'>
+                <TitleCard >agendamento</TitleCard>
+              </Stack>
+                <DividerCard />
+              <List dense={false}>
+              
+              <ListItemCard>
+                  <InfoCardName>
+                    Número do atendimento:
+                  </InfoCardName> {router.query.id ? (<InfoCardText >{router.query.id}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                  <InfoCardName>
+                    Data da visita:
+                  </InfoCardName>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={ptBR}>
+                    <DateTimePickerCard
+                      format="DD/MM/YYYY hh:mm"
+                      slotProps={{ textField: { size: 'small' } }}
+                      value={visitDate}
+                      // slotProps={{
+                      //   textField: {
+                      //     helperText: 'DD / MM / YYYY',
+                      //   },
+                      // }}
+                      onChange={(newValue: any) => {
+                        console.log(newValue)
+                        setVisitDate(newValue)
+                      }}
+                    />
+                    </LocalizationProvider>
+                  {/* {client?.cpf ? (<InfoCardText>{client?.cpf}</InfoCardText>) : (
+                     <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )} */}
+              </ListItemCard>
+              
+            </List>
+              </Paper>
+              <Paper
+                    sx={{
+                      p: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                     <Stack direction='row'>
+              <TitleCard >Consultor técnico</TitleCard>
+              </Stack>
+                <DividerCard />
+            <List dense={false}>
+              
+              <ListItemCard>
+                  <InfoCardName>
+                    Nome:
+                  </InfoCardName> {technicalConsultant?.name ? (<InfoCardText >{technicalConsultant?.name}</InfoCardText>) : (
+                    <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+              <ListItemCard>
+                  <InfoCardName>
+                    Código consultor:
+                  </InfoCardName> {technicalConsultant?.id ? (<InfoCardText>{technicalConsultant?.id }</InfoCardText>) : (
+                     <InfoCardText width='100%'><Skeleton variant="text" sx={{ fontSize: '1rem', lineHeight: 1.5 }} width='100%' /></InfoCardText>
+                  )}
+              </ListItemCard>
+            </List>
+              </Paper>
+            </Stack>
+          </Grid>
+
+        </Grid>
+    </Container>
+    
   );
 }
 
---
+++ src/styles/pages/service-schedules/stylesEdit.ts
@@ -0,0 +1,77 @@
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import { alpha, styled } from '@mui/material/styles';
+import IconButton from "@mui/material/IconButton";
+import { teal } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import { Divider, Typography } from '@mui/material';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+// export const ListCard = styled(List)(({ theme }) => ({
+//   padding: 1
+// }))
+export const ListItemCard = styled(ListItem)(({ theme }) => ({
+  margin: '0 0 15px 0',
+  padding: 0,
+  '&:last-child': {
+    margin: 0
+  } 
+}))
+export const TitleCard = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.5,
+  color: '#0E948B',
+  fontWeight: 'bold',
+  textTransform: 'uppercase'
+}))
+export const DividerCard = styled(Divider)(({ theme }) => ({
+  margin: '10px 0'
+}))
+export const InfoCardName = styled(Typography)(({ theme }) => ({
+  fontWeight: 'bold',
+  marginRight: 3
+}))
+export const InfoCardText = styled(Typography)(({ theme }) => ({
+  lineHeight: 1.5,
+}))
+
+
+
+
+
+export const ButtonLeft = styled(Button)(({ theme }) => ({
+  color: 'white',
+  background: '#0E948B',
+  borderRadius: 4,
+  borderTopRightRadius: 0,
+  borderBottomRightRadius: 0,
+  padding: '5px 16px',
+  flex: 1,
+  textTransform: 'none',
+  '&:hover': {
+    background: '#1ACABA',
+  }
+  
+}))
+export const ButtonCenter = styled(IconButton)(({ theme }) => ({
+  color: 'white',
+  background: '#0E948B',
+  borderRadius: 0,
+  '&:hover': {
+    background: '#1ACABA',
+  }
+}))
+export const ButtonRight = styled(Button)(({ theme }) => ({
+  color: 'white',
+  background: '#0E948B',
+  borderRadius: 4,
+  borderTopLeftRadius: 0,
+  borderBottomLeftRadius: 0,
+  padding: '10px 20px',
+  textTransform: 'none',
+  '&:hover': {
+    background: '#1ACABA',
+  }
+}))
+
+export const DateTimePickerCard = styled(DateTimePicker)(({ theme }) => ({
+}))
